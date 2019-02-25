96631a53a9750d12622eb3a9217e071864a62d82
kxeilakos <kxeilakos@cite.gr>
2019-02-25 12:51:08 +0200

    update db context class

--- Formula1MyLive_Project/Formula1MyLive/Services/DbContextService.cs
+++ Formula1MyLive_Project/Formula1MyLive/Services/DbContextService.cs
@@ -8,7 +8,18 @@ namespace Formula1MyLive.Services
 	{
 		public DbContextService(DbContextOptions<DbContextService> options) : base(options) { }
 
-		public DbSet<Campaign> Campaign { get; set; }
-		public DbSet<CampaignContactGroup> CampaignContactGroup { get; set; }
+		public DbSet<Circuit> Circuit { get; set; }
+		public DbSet<Constructor> Constructor { get; set; }
+		public DbSet<ConstructorResult> ConstructorResult { get; set; }
+		public DbSet<ConstructorStanding> ConstructorStanding { get; set; }
+		public DbSet<Driver> Driver { get; set; }
+		public DbSet<DriverStanding> DriverStanding { get; set; }
+		public DbSet<LapTime> LapTime { get; set; }
+		public DbSet<PitStop> PitStop { get; set; }
+		public DbSet<Qualifying> Qualifying { get; set; }
+		public DbSet<Race> Race { get; set; }
+		public DbSet<Race> Result { get; set; }
+		public DbSet<Race> Season { get; set; }
+		public DbSet<Race> Status { get; set; }
 	}
 }

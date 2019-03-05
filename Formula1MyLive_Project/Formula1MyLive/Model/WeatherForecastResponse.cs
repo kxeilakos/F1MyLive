ae9310d0b5305ccf0b668f1b0a313777ecb56cdb
kxeilakos <kxeilakos@cite.gr>
2019-03-05 17:02:47 +0200

    enrich DarkSky response with data

--- Formula1MyLive_Project/Formula1MyLive/Model/WeatherForecastResponse.cs
+++ Formula1MyLive_Project/Formula1MyLive/Model/WeatherForecastResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,25 +8,60 @@ namespace Formula1MyLive.Model
 {
 	public class WeatherForecastResponse
 	{
-		string Status { get; set; }
-		string Message { get; set; }
-		short Offset { get; set; }
-		string Latitude { get; set; }
-		string Longitude { get; set; }
-		string Timezone { get; set; }
-		string Daily { get; set; }
-		List<DailyData> Data { get; set; }
+
+		public string Status { get; set; }
+		public string Message { get; set; }
+
+		[JsonProperty(PropertyName = "offset")]
+		public short Offset { get; set; }
+
+		[JsonProperty(PropertyName = "latitude")]
+		public string Latitude { get; set; }
+
+		[JsonProperty(PropertyName = "longitude")]
+		public string Longitude { get; set; }
+
+		[JsonProperty(PropertyName = "timezone")]
+		public string Timezone { get; set; }
+
+		[JsonProperty(PropertyName = "daily")]
+		public DailyDataItems Daily { get; set; }
+		
+	}
+
+	public class DailyDataItems
+	{
+		[JsonProperty(PropertyName = "data")]
+		public List<DailyData> Data { get; set; }
 	}
 
 	public class DailyData
 	{
+		[JsonProperty(PropertyName = "time")]
 		public long Time { get; set; }
+
+		[JsonProperty(PropertyName = "summary")]
 		public string Summary { get; set; }
+
+		[JsonProperty(PropertyName = "icon")]
 		public string Icon { get; set; }
+
+		[JsonProperty(PropertyName = "temperatureHigh")]
 		public double TemperatureHigh { get; set; }
+
+		[JsonProperty(PropertyName = "temperatureLow")]
 		public double TemperatureLow { get; set; }
+
+		[JsonProperty(PropertyName = "apparentTemperatureHigh")]
 		public double ApparentTemperatureHigh { get; set; }
+
+		[JsonProperty(PropertyName = "apparentTemperatureLow")]
 		public double ApparentTemperatureLow { get; set; }
 
+		[JsonProperty(PropertyName = "humidity")]
+		public double Humidity { get; set; }
+
+		[JsonProperty(PropertyName = "windSpeed")]
+		public double wWndSpeed { get; set; }
 	}
 }
